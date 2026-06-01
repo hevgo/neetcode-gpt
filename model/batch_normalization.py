8e56e1f5d5fd261116428203df9c36433a40fa38
Yuxuan Jing <jingyuxuan0426@gmail.com>
2026-05-31 18:30:51 -0700

    Update Batch normalization (PyTorch 3/4)

--- model/batch_normalization.py
+++ model/batch_normalization.py
@@ -8,28 +8,31 @@ class Solution:
                    momentum: float, eps: float, training: bool) -> Tuple[List[List[float]], List[float], List[float]]:
         # During training: normalize using batch statistics, then update running stats
         # During inference: normalize using running stats (no batch stats needed)
-        # Apply affine transform: y = gamma * x_hat + beta
-        # Return (y, running_mean, running_var), all rounded to 4 decimals as lists
         
-        X = np.array(x)
-        gamman = np.array(gamma)
+        x = np.array(x)
+        gamma = np.array(gamma)
         beta = np.array(beta)
         running_mean = np.array(running_mean)
         running_var = np.array(running_var)
 
-        n_batch, n_feture = X.shape
+        B, N = x.shape
 
-        if training:
-            mu = np.mean(X, axis = 0)
-            var = np.var(X, axis = 0)
-            x_hat = (X-mu)/np.sqrt(var+eps)
-            
-            running_mean = (1 - momentum) * running_mean + momentum * mu
-            running_var = (1 - momentum) * running_var + momentum * var
         
+        if training:
+            mu = (1/B) * np.sum(x, axis = 0)
+            var = (1/B) * np.sum((x - mu)**2, axis = 0)
+            x_hat = (x - mu)/np.sqrt(var + eps)
+            running_mean = (1-momentum) * running_mean + momentum * mu
+            running_var = (1-momentum) * running_var + momentum * var
         else:
-            x_hat = (X - running_mean)/np.sqrt(running_var + eps)
+            x_hat = (x - running_mean)/np.sqrt(running_var + eps)
+
         
         y = gamma * x_hat + beta
 
-        return np.round(y, 4), np.round(running_mean, 4), np.round(running_var, 4)
+        return (np.round(y, 4).tolist(), np.round(running_mean, 4).tolist(), np.round(running_var, 4).tolist())
+
+
+
+
+        
